7554a9783f3b023ce5ef6acf538623f794604eae
Erick Dias <140857951+erickdias10@users.noreply.github.com>
2025-01-07 18:50:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ logger = logging.getLogger()
 
 # Função para carregar o modelo spaCy
 def load_spacy_model():
-    model_name = "pt_core_news_lg"
+    model_name = "pt_core_news_sm"
     if not spacy.util.is_package(model_name):
         from spacy.cli import download
         with st.spinner("Baixando modelo spaCy (isso pode levar alguns minutos)..."):
